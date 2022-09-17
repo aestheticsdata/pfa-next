ada776bf84603dc75199d41a2448fa94dcced8dd
Aestheticsdata <scb_international@yahoo.com>
2022-09-17 19:38:54 +0200

    about page

--- pfa-client-next/pages/about/index.tsx
+++ pfa-client-next/pages/about/index.tsx
@@ -2,8 +2,13 @@ import Layout from "@src/components/shared/Layout";
 
 const About = () => {
   return (
-    <Layout>
-      <div>About</div>
+    <Layout isLogin>
+      <div className="flex flex-col items-center w-96 space-y-8 mt-28 rounded bg-gradient-to-br from-teal-300 to-sky-500 py-3 font-smooch shadow-lg">
+        <div>Site hébergé chez OVH SAS</div>
+        <div>Siège social : 2 rue Kellermann - 59100 Roubaix - France</div>
+        <div>Code APE 2620Z</div>
+        <div>N° TVA : FR 22 424 761 419</div>
+      </div>
     </Layout>
   );
 };
