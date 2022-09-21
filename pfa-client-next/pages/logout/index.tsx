2d13cc60de8e327c87e0a2b4017f50f96a4f2985
Aestheticsdata <scb_international@yahoo.com>
2022-09-21 21:03:35 +0200

    logout bug fix

--- pfa-client-next/pages/logout/index.tsx
+++ pfa-client-next/pages/logout/index.tsx
@@ -1,12 +1,16 @@
-import { useUserStore } from "@auth/store/userStore";
-import { useAuthStore } from "@auth/store/authStore";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { useQueryClient } from "react-query";
+import { useAuthStore } from "@auth/store/authStore";
+import { useUserStore } from "@auth/store/userStore";
 
 const Logout = () => {
   const userStore = useUserStore();
   const authStore = useAuthStore();
   const router = useRouter();
+
+  const queryClient = useQueryClient();
+  queryClient.clear();
 
   useEffect(() => {
     authStore.setToken(null);
