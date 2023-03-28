3239ba5280c9eba74464a6385448c56c155bed45
Aestheticsdata <scb_international@yahoo.com>
2023-03-28 16:37:47 +0200

    post recurring prisma to mysql2 - wip

--- src/routes/controllers/recurrings/postRecurringController.js
+++ src/routes/controllers/recurrings/postRecurringController.js
@@ -1,7 +1,43 @@
-const prisma = require('../../../db/dbInit');
+// const prisma = require('../../../db/dbInit');
+// const { v1: uuidv1 } = require('uuid');
+// const createError = require('http-errors');
+//
+//
+// module.exports = async (req, res, next) => {
+//   const {
+//     userID,
+//     start: dateFrom,
+//     end: dateTo,
+//     label,
+//     amount,
+//     currency,
+//   } = req.body;
+//
+//   if (!amount || !label) {
+//     return next(createError(500, 'Please enter amount and label'));
+//   }
+//
+//   await prisma.recurrings.create({
+//     data: {
+//       ID: uuidv1(),
+//       userID,
+//       dateFrom: new Date(dateFrom),
+//       dateTo: new Date(dateTo),
+//       label,
+//       amount,
+//       currency,
+//       itemType: 'recurring',
+//     },
+//   });
+//   res.json('new category added');
+// };
+
+
+
+
+const dbConnection = require('../../../db/dbinitmysql');
 const { v1: uuidv1 } = require('uuid');
 const createError = require('http-errors');
-
 
 module.exports = async (req, res, next) => {
   const {
@@ -17,18 +53,16 @@ module.exports = async (req, res, next) => {
     return next(createError(500, 'Please enter amount and label'));
   }
 
-  await prisma.recurrings.create({
-    data: {
-      ID: uuidv1(),
-      userID,
-      dateFrom: new Date(dateFrom),
-      dateTo: new Date(dateTo),
-      label,
-      amount,
-      currency,
-      itemType: 'recurring',
-    },
-  });
-  res.json('new category added');
+  const sql = `
+    INSERT INTO Recurrings (ID, userID, dateFrom, dateTo, label, amount, currency, itemType)
+    VALUES ("${uuidv1()}", "${userID}", "${dateFrom}", "${dateTo}", "${label}", "${amount}", "${currency}", "recurring")
+  `;
+
+  dbConnection.query(
+    sql,
+    () => {
+      res.json("new recurring added")
+    }
+  );
 };
 
