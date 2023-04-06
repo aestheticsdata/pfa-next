7e77d6ce5e2ba4e261ac6407d6fc483ac3304738
Aestheticsdata <scb_international@yahoo.com>
2023-04-06 23:36:24 +0200

    user signup prisma to mysql2

--- src/routes/controllers/users/addUserController.js
+++ src/routes/controllers/users/addUserController.js
@@ -1,8 +1,9 @@
-const signIn = require('./helpers/signInHelper');
 const bcrypt = require('bcryptjs');
-const prisma = require('../../../db/dbInit');
 const { v1: uuidv1 } = require('uuid');
 const createError = require('http-errors');
+const { format } = require('date-fns');
+const dbConnection = require('../../../db/dbinitmysql');
+const signIn = require('./helpers/signInHelper');
 
 
 module.exports = async (req, res, next) => {
@@ -19,32 +20,54 @@ module.exports = async (req, res, next) => {
     return next(createError(500, 'Please enter all fields'));
   }
 
-  let user = await prisma.users.findUnique({ where: { email } });
-  if (user) { return next(createError(500, 'Email already exists')); }
+  const sqlUser = `
+    SELECT * FROM Users
+    WHERE email="${email}";
+  `;
+  dbConnection.query(
+    sqlUser,
+    (err, users) => {
+      if (users.length > 0) { return next(createError(500, 'Email already exists')); }
 
-  const newUser = {
-    ID: uuidv1(),
-    name,
-    email,
-    password,
-    baseCurrency,
-    registerDate,
-    language,
-  };
+      const newUser = {
+        ID: uuidv1(),
+        name,
+        email,
+        password,
+        baseCurrency,
+        registerDate,
+        language,
+      };
 
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) console.error('There was an error during salt', err);
-    else {
-      bcrypt.hash(newUser.password, salt, async (err, hash) => {
-        if (err) console.error('There was an error during hash', err);
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) console.error('There was an error during salt', err);
         else {
-          newUser.password = hash;
-          user = await prisma.users.create({ data: newUser });
-          signIn(res, user);
+          bcrypt.hash(newUser.password, salt, async (err, hash) => {
+            if (err) console.error('There was an error during hash', err);
+            else {
+              newUser.password = hash;
+              console.log("registerDate", format(new Date(registerDate), "yyyy-MM-dd HH:mm:ss"));
+              const sqlCreateUser = `
+                INSERT INTO Users (ID, name, password, email, registerDate, language, baseCurrency)
+                VALUES ("${newUser.ID}", "${newUser.name}", "${newUser.password}", "${newUser.email}", "${format(new Date(registerDate), "yyyy-MM-dd HH:mm:ss")}", "fr", "EUR");`;
+              dbConnection.query(
+                sqlCreateUser,
+                () => {
+                  signIn(res, {
+                    id: newUser.ID,
+                    name: newUser.name,
+                    email: newUser.email,
+                    language: "fr",
+                    baseCurrency: "EUR",
+                  });
+                }
+              );
+            }
+          });
         }
       });
     }
-  });
+  )
 };
 
 
--- src/routes/controllers/users/resetPasswordController.js
+++ src/routes/controllers/users/resetPasswordController.js
@@ -1,49 +1,49 @@
-const prisma = require('../../../db/dbInit');
-
-const bcrypt = require('bcryptjs');
-const passwordgenerator = require('generate-password');
-
-// Sendinblue
-const SibApiV3Sdk = require('sib-api-v3-sdk');
-let defaultClient = SibApiV3Sdk.ApiClient.instance;
-let apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = process.env.SENDINBLUE_APIKEY;
-let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-// Sendinblue end
-
-
-const createError = require('http-errors');
-
-
-module.exports = async (req, res, next) => {
-  const { email, subject, changedPassword } = req.body;
-
-  const newPassword = changedPassword || passwordgenerator.generate({
-    length: 10,
-    numbers: true,
-  });
-
-  sendSmtpEmail.subject = subject;
-  sendSmtpEmail.htmlContent = `votre nouveau mot de passe: ${newPassword}`;
-  sendSmtpEmail.sender = {"name":"HXF finance","email":"hxf.finance@gmail.com"};
-  sendSmtpEmail.to = [{"email":email, "name":email}];
-  sendSmtpEmail.replyTo = {"email":"hxf.finance@gmail.com", "name":"HXF Finance"};
-
-  const user = await prisma.users.findUnique({ where: { email } });
-  if (user === null) return next(createError(500, 'no users registered with this email'));
-
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) console.error('There was an error during salt', err);
-    else {
-      bcrypt.hash(newPassword, salt, async (err, hash) => {
-        if (err) console.error('There was an error during hash', err);
-        else {
-          await prisma.users.update({ where: { email }, data: { password: hash } })
-          await apiInstance.sendTransacEmail(sendSmtpEmail);
-          res.json('sendinblue success');
-        }
-      });
-    }
-  });
-};
+// const prisma = require('../../../db/dbInit');
+//
+// const bcrypt = require('bcryptjs');
+// const passwordgenerator = require('generate-password');
+//
+// // Sendinblue
+// const SibApiV3Sdk = require('sib-api-v3-sdk');
+// let defaultClient = SibApiV3Sdk.ApiClient.instance;
+// let apiKey = defaultClient.authentications['api-key'];
+// apiKey.apiKey = process.env.SENDINBLUE_APIKEY;
+// let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+// let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+// // Sendinblue end
+//
+//
+// const createError = require('http-errors');
+//
+//
+// module.exports = async (req, res, next) => {
+//   const { email, subject, changedPassword } = req.body;
+//
+//   const newPassword = changedPassword || passwordgenerator.generate({
+//     length: 10,
+//     numbers: true,
+//   });
+//
+//   sendSmtpEmail.subject = subject;
+//   sendSmtpEmail.htmlContent = `votre nouveau mot de passe: ${newPassword}`;
+//   sendSmtpEmail.sender = {"name":"HXF finance","email":"hxf.finance@gmail.com"};
+//   sendSmtpEmail.to = [{"email":email, "name":email}];
+//   sendSmtpEmail.replyTo = {"email":"hxf.finance@gmail.com", "name":"HXF Finance"};
+//
+//   const user = await prisma.users.findUnique({ where: { email } });
+//   if (user === null) return next(createError(500, 'no users registered with this email'));
+//
+//   bcrypt.genSalt(10, (err, salt) => {
+//     if (err) console.error('There was an error during salt', err);
+//     else {
+//       bcrypt.hash(newPassword, salt, async (err, hash) => {
+//         if (err) console.error('There was an error during hash', err);
+//         else {
+//           await prisma.users.update({ where: { email }, data: { password: hash } })
+//           await apiInstance.sendTransacEmail(sendSmtpEmail);
+//           res.json('sendinblue success');
+//         }
+//       });
+//     }
+//   });
+// };
