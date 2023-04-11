9a647ee320de35bc493150716e7e5d5cb01febe0
Aestheticsdata <scb_international@yahoo.com>
2023-04-11 20:02:06 +0200

    bug fix add new user

--- src/routes/controllers/users/addUserController.js
+++ src/routes/controllers/users/addUserController.js
@@ -54,7 +54,7 @@ module.exports = async (req, res, next) => {
                 sqlCreateUser,
                 () => {
                   signIn(res, {
-                    id: newUser.ID,
+                    ID: newUser.ID,
                     name: newUser.name,
                     email: newUser.email,
                     language: "fr",
