d9d2935b8384497da0c0eec0922e29c44995d00f
Aestheticsdata <scb_international@yahoo.com>
2024-04-28 14:50:29 +0200

    revert logout changes

--- pfa-client-next/pages/logout/index.tsx
+++ pfa-client-next/pages/logout/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useQueryClient } from "react-query";
 import { useAuthStore } from "@auth/store/authStore";
 import { useUserStore } from "@auth/store/userStore";
@@ -8,7 +8,6 @@ const Logout = () => {
   const userStore = useUserStore();
   const authStore = useAuthStore();
   const router = useRouter();
-  const [isRedirected, setIsRedirected] = useState(false);
 
   const queryClient = useQueryClient();
   queryClient.clear();
@@ -16,10 +15,7 @@ const Logout = () => {
   useEffect(() => {
     authStore.setToken(null);
     userStore.setUser(null);
-    if (!isRedirected) { // https://stackoverflow.com/a/73344411/5671836
-      router.push("/login");
-      setIsRedirected(true);
-    }
+    router.push("/login");
   });
 
   return <></>;
